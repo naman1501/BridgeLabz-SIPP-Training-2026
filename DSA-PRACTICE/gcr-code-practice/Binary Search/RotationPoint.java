f51c60a88a26541a348e09184c981320a0b36ec4
naman1501 <145916454+naman1501@users.noreply.github.com>
2026-07-16 11:07:19 +0530

    [naman].add

---
+++ DSA-PRACTICE/gcr-code-practice/Binary Search/RotationPoint.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class RotationPoint{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int left = 0;
+        int right = n - 1;
+
+        while(left < right){
+
+            int mid = left + (right - left) / 2;
+
+            if(arr[mid] > arr[right])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+
+        System.out.println("Rotation Index = " + left);
+        System.out.println("Minimum Element = " + arr[left]);
+    }
+}
